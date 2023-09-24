f68665fe4c991cd26f2b66f9cbf7dc2d5d430f9e
Artem Boxberger <a.boksberger@g.nsu.ru>
2023-09-24 14:45:47 +0700

    initialise

---
+++ src/main/java/ru/nsu/boxberger/shift_task/ShiftTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.boxberger.shift_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nsu/boxberger/shift_task/ShiftTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.boxberger.shift_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
